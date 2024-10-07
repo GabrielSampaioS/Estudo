dee741cf6342344b2b1857f1db649b8c4e510f32
Gabriel Sampaio <gabrielssampaio42@gmail.com>
2024-10-07 06:53:42 -0300

    Noded.js com SQL

--- nodeJs/NodeJS Baico/6. SQL/Aula 1/index.js
+++ nodeJs/NodeJS Baico/6. SQL/Aula 1/index.js
@@ -1,22 +1,30 @@
 // Configurando Server
 
-const express = require('express')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const mysql = require('mysql')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const mysql = require('mysql');
 
-const app = express()
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true //Conseguir pegar o body
+    })
+)
+
+app.use(express.json())
 
 // Definir partials
 const hbs = exphbs.create({
     partialsDir: ['views/partials'],
-})
+});
 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Conseguir visualizar a pasta public
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configurando Server -- FIM
 
@@ -25,24 +33,127 @@ const conn = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'NodeMySQL'
-})
+});
 
 // Conectar ao banco de dados
-conn.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err)
-        return
+conn.connect(function (erro) { // Certifique-se de usar 'erro' aqui
+    if (erro) {
+        console.log(erro);
+        return; // Para evitar continuar caso haja erro
     }
-    console.log('Conectado ao banco de dados MySQL')
+
+    console.log('Conectado ao SQL');
+
+    app.listen(3000, () => {
+        console.log('Servidor rodando na porta 3000');
+    });
+});
+
+// ROTAS
+
+app.get('/books', (req, res) => {
+    const query = "SELECT * FROM BOOKS"
+
+    conn.query(query, function (err, data) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Erro ao visualizar os livros.');
+        }
+
+        const books = data
+
+        res.render('books', { books })
+
+    })
+
+
+})
+
+app.get('/book/edit/:id', (req, res) => {
+
+    const id = req.params.id
+    const query = `SELECT * FROM BOOKS WHERE ID = ${id}`
+
+
+    conn.query(query, function (err, data) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send('Erro ao editar o livro.');
+        }
+        const book = data[0]
+        console.log[book]
+        res.render('bookEdit', { book })
+    })
+
+})
+
+app.post('/book/updatebook', (req, res) => {
+    const id = req.body.id
+    const title = req.body.title
+    const pageqty = req.body.pagesqty
+
+
+    const query = `UPDATE books SET nome = '${title}', qtdPaginas = '${pageqty}' WHERE id = ${id}`
+
+    conn.query(query, function (err) {
+
+        if (err) {
+            console.log(err)
+            return res.status(500).send('Erro ao atualizar o livro.');
+        }
+
+        res.redirect(`/book/${id}`)
+    })
+})
+
+app.get('/book/:id', (req, res) => {
+    const id = req.params.id
+
+    const query = `SELECT * from BOOKS WHERE ID = ${id}`
+
+    conn.query(query, function (err, data) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send('Erro ao visualizar o livro.');
+        }
+        const book = data[0]
+        res.render('book', { book })
+    })
+
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title; // Corrigido para 'title'
+    const pageqty = req.body.pagesqty; // Corrigido para 'pagesqty'
+
+    //Vulneravel a SQL Injection
+    const query = `INSERT INTO BOOKS (nome, qtdPaginas) VALUES ('${title}', '${pageqty}')`;
+
+    conn.query(query, function (err) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Erro ao inserir o livro.'); // Resposta em caso de erro
+        }
+        res.redirect('/books');
+    });
+});
+
+app.post('/book/remove/:id', (req, res) => {
+    const id = req.params.id
+
+    const query = `DELETE FROM books WHERE ID = ${id}`
+
+    conn.query(query, function(err){
+        if(err){
+            console.log(err)
+            return res.status(500).send('Erro ao deletar livro.'); // Resposta em caso de erro
+        }
+
+        res.redirect('/books')
+    })
 })
 
 // Rota principal
 app.get('/', (req, res) => {
-    res.render('home')
-    console.log('Teste')
-})
-
-// Inicializando o servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
-})
+    res.render('home');
+});
