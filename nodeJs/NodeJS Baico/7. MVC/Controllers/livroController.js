e5a4d62e38a81a58be056865f6a63a795fde193c
Gabriel Sampaio <gabrielssampaio42@gmail.com>
2024-10-22 07:15:00 -0300

    Finalizazndo primeiro MVC

--- nodeJs/NodeJS Baico/7. MVC/Controllers/livroController.js
+++ nodeJs/NodeJS Baico/7. MVC/Controllers/livroController.js
@@ -5,10 +5,95 @@ exports.listarLivros = async (req, res) => {
     try{
         const livros = await Livro.getAll();
         res.render('livros/livros', {livros})
-        console.log(livros)
     } catch(err){
         console.error(err);
         res.status(500).send('Erro ao buscar livros')
     }
 
 };
+
+exports.CadastrarLivro = async (req, res) => {
+    try{
+        res.render('livros/CadastrarLivro')
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Erro ao buscar o furmulario de livros')
+    }
+
+};
+
+exports.listaLivro = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const book = await Livro.getOne(id);
+        res.render('livros/livro', {book})
+    }catch(err){
+        console.log(err);
+        res.status(500).send('Erro ao buscar livro')
+    }
+};
+
+exports.EditarLivro = async (req, res) => {
+    try {
+        const id = req.params.id;
+        if (!id) {
+            return res.status(400).send('ID do livro é necessário'); // Responde com erro se o ID não estiver presente
+        }
+
+        const book = await Livro.getOne(id)
+        res.render('livros/EditarLivro', {book})
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Erro ao editar');
+    }
+};
+
+exports.InserirLivro = async (req, res) => {
+    try {
+        const title = req.body.title
+        const pageqt = req.body.pagesqty
+
+        try{
+            Livro.create(title, pageqt)
+            res.redirect('/livros'); 
+        }catch (err){
+            console.log(err)
+        }
+
+
+    }catch(err) {
+        console.log(err)
+        res.status(500).send('Erro ao inserir novo livro')
+    }
+};
+
+exports.updatelivro = async(req, res) => {
+    try{
+        const title = req.body.title
+        const pageqt = req.body.pagesqty
+        const id = req.body.id
+
+        try{
+            Livro.update(title, pageqt, id)
+            res.redirect('/livros'); 
+        } catch {
+            console.log(err)
+        }
+    } catch {
+        console.log(err)
+        res.status(500).send('Erro ao inserir novo livro')
+    }
+
+}
+
+exports.deletarlivro = async (req, res) => {
+    try {
+        const id = req.params.id; // Use req.params.id
+        await Livro.deleta(id);
+        res.redirect('/livros');
+    } catch (err) {
+        console.log('Erro ao deletar livro:', err);
+        res.status(500).send('Erro ao deletar o livro');
+    }
+};
+
--- nodeJs/NodeJS Baico/7. MVC/routes/autorRoutes.js
+++ nodeJs/NodeJS Baico/7. MVC/routes/autorRoutes.js
@@ -1,4 +1,3 @@
-// routes/autorRoutes.js
 const express = require('express');
 const router = express.Router();
 const autorController = require('../controllers/autorController');
@@ -7,3 +6,5 @@ router.get('/', autorController.listarAutores); // Rota para listar autores
 router.post('/adicionar', autorController.adicionarAutor); // Rota para adicionar autor
 
 module.exports = router;
+
+2,054
